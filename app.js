5dc75146c0ec623a47770396fe3a5c38dff645e6
SagarYelgondHyniva <103254780+sagaryelgondhyniva@users.noreply.github.com>
2023-09-22 16:48:08 +0530

    download commited

--- app.js
+++ app.js
@@ -68,3 +68,13 @@ module.exports.handle = async (event) => {
     };
   }
 };		  
+
+exports.hello = async (event, context) => {
+  console.log(`Hi from Node.js ${process.version} on Lambda!`)
+  S3.getObject({Bucket: BUCKET, Key: 'pic1 - Copy (1).pdf'}).promise().then( data =>{
+    return {
+      statusCode: 200,
+      body: data
+  }})
+}
+
